fa65e752b6bfb6d64259598e9b9165c959d465a1
gseich <gseich@iastate.edu>
2023-10-17 21:13:50 -0500

    Updated event handlers

--- src/Button.js
+++ src/Button.js
@@ -1,34 +1,17 @@
 import React from 'react';
 import './Button.css';
 
-// export class Button extends React.Component {
-//     constructor(props) {
-//         super(props);
-
-//         this.state = {
-//             value : props.symbol    // Value of the button
-//         }
-//     }
-
-//     handleClick() {
-
-//     }
-
-//     render() {
-//         return (
-//             <button>{this.props.symbol}</button>
-//         )
-//     }
-// }
-
 export function Button(props) {
 
 
     return (
         <button 
-            
+            className='button'
+            id={props.id}
+            value={props.value}
+            onClick={props.clickHandler(props.value)}
         >
-
+            {props.value}
         </button>
     )
 }
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState } from "react";
 import { Button } from './Button.js';
 import './Calculator.css';
 
@@ -8,81 +8,110 @@ export function Calculator() {
     const buttons = [
         {
             id : "zero",
-            symbol : 0
+            value : 0
         },
         {
             id: "one",
-            symbol : 1
+            value : 1
         }, 
         {
             id : "two",
-            symbol : 2
+            value : 2
         }, 
         {
             id : "three",
-            symbol : 3
+            value : 3
         },
         {
             id : "four",
-            symbol : 4
+            value : 4
         },
         {
             id : "five",
-            symbol : 5
+            value : 5
         }, 
         {
             id : "six",
-            symbol : 6
+            value : 6
         },
         {
             id : "seven",
-            symbol : 7
+            value : 7
         },
         {
             id : "eight",
-            symbol : 8
+            value : 8
         },
         {
             id : "nine",
-            symbol : 9
+            value : 9
         },
         {
             id : "add",
-            symbol : "+"
+            value : "+"
         },
         {
             id : "subtract",
-            symbol : "-",
+            value : "-",
         },
         {
             id : "multiply",
-            symbol : "*"
+            value : "*"
         },
         {
             id : "divide",
-            symbol : "/"
+            value : "/"
         },
         {
             id : "decimal",
-            symbol : "."
+            value : "."
         }, 
         {
             id : "clear",
-            symbol : "C"
+            value : "C"
         },
         {
             id : "equals",
-            symbol : "="
+            value : "="
         }
     ];
+
+    const [ output, setOutput ] = useState('');
+
+    // Handle Clicks
+    const updateDisplay = (value) => {
+        // setOutput(...prev += value);
+        // console.log('A click has occurred');
+        if(output === undefined || output === 'Error') {
+            setOutput(value);
+        }
+        else {
+            setOutput((prevValue) => (prevValue + value).toString());
+        }
+        console.log('Updated display');
+    } 
+
+    // Handle Equals
+    const calculate = () => {
+        const expression = output.toString();
+        
+    }
+
+    // Handle Clear
+    const clear = () => {
+        setOutput('');
+        console.log('Output Cleared!');
+    }
 
     return (
         <>
             <div id="calculator">
-                <div id="display"></div>
+                <div id="display">{output}</div>
                 <div id="ui">
                     {
-
+                        buttons.map((btn) => (
+                            <Button key={btn.id} id={btn.id} value={btn.value}  clickHandler={updateDisplay} />
+                        ))
                     }
                 </div>
             </div>
@@ -90,87 +119,4 @@ export function Calculator() {
     )
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export class Calculator extends React.Component {
-    
-//     constructor(props) {
-//         super(props);
-
-//         this.state = {
-//             currentOutput : 0
-//         };
-
-//         this.calculate = this.handleClick.bind(this);
-//         this.handleClick = this.handleClick.bind(this);
-//     }
-
-//     handleClick(event) {
-
-//     }
-
-//     calculate() {
-
-//     }
-
-//     render() {
-//         return (
-//             <div id="calculator">
-                
-//                 <div id="ui">
-//                     <Button className="button" symbol="C" id="clear" />
-//                     <Button className="button" symbol="/" id="divide" />
-//                     <Button className="button" symbol="*" id="multiply" />
-//                     <Button className="button" symbol="-" id="subtract" />
-//                     <Button className="button" symbol="7" id="seven" />
-//                     <Button className="button" symbol="8" id="eight" />
-//                     <Button className="button" symbol="9" id="nine" />
-//                     <Button className="button" symbol="+" id="add" />
-//                     <Button className="button" symbol="4" id="four" />
-//                     <Button className="button" symbol="5" id="five" />
-//                     <Button className="button" symbol="6" id="six" />
-//                     <Button className="button" symbol="1" id="one" />
-//                     <Button className="button" symbol="2" id="two" />
-//                     <Button className="button" symbol="3" id="three" />
-//                     <Button className="button" symbol="=" id="equals" />
-//                     <Button className="button" symbol="0" id="zero" />
-//                     <Button className="button" symbol="." id="decimal" />
-//                 </div>
-
-//             </div>
-//         );
-//     }
-// }
-
-// export default Calculator;
+export default Calculator;
